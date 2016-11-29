5494536be8632ada86ce4173b2c1faf933a0fd32
Wyatt McGehee <wyatt.mcgehee@wsu.edu>
2016-11-29 12:10:13 -0800

    Basic Paused round screen. Needs to be used in an active round.

--- Mobile-Rounds/App.xaml.cs
+++ Mobile-Rounds/App.xaml.cs
@@ -83,7 +83,7 @@ namespace Mobile_Rounds
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(ReadingInput), e.Arguments);
+                    rootFrame.Navigate(typeof(HomeScreen), e.Arguments);
                 }
 
                 // Ensure the current window is active
