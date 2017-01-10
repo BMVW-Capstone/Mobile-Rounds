3f1a86a41b84d3e598df7abb6f5f82ba6d5b4c68
Tyler Vanderhoef <tyler.vanderhoef@vibrantusa.com>
2017-01-09 19:10:38 -0800

    Re-added tests for the shared PCL.

---
+++ Shared/Mobile-Rounds.ViewModels.Tests/Shared/Commands/StartRoundCommandTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mobile_Rounds.ViewModels.Shared.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_Rounds.ViewModels.Tests.Shared.Commands
+{
+    [TestClass]
+    public class StartRoundCommandTests
+    {
+        [TestMethod]
+        public void CanExecuteIsTrue()
+        {
+            var cmd = new StartRoundCommand();
+            Assert.IsTrue(cmd.CanExecute(null));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void ExecuteThrowsNotImplemented()
+        {
+            var cmd = new StartRoundCommand();
+            cmd.Execute(null);
+        }
+    }
+}
