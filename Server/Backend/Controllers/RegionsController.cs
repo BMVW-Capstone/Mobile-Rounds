d509c3a085ade428a72ccefd57cd3f1da13582cb
tvand7093 <tyler.j.vanderhoef@gmail.com>
2017-01-12 02:38:02 -0800

    Removed placeholder actions for login tests.

--- Server/Backend/Controllers/RegionsController.cs
+++ Server/Backend/Controllers/RegionsController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
@@ -23,24 +24,7 @@ namespace Backend.Controllers
             datasource = new RegionRepository(database);
         }
 
-        [Route("open")]
-        [AllowAnonymous]
-        public async Task<IHttpActionResult> GetAnonymous()
-        {
-            var user = HttpContext.Current.User;
-            var role = user.IsInRole("Developers");
-
-            var results = await datasource.GetAsync();
-
-            return Ok(new
-            {
-                db = results,
-                user = user.Identity.Name,
-                isDeveloper = role
-            });
-        }
-
-        [Route("closed")]
+        [Route("")]
         public async Task<IHttpActionResult> Get()
         {
             var user = HttpContext.Current.User;
