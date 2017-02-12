fdab6b796035126abe0c9cfcb0649ff92f78ddbb
Tyler Vanderhoef <tyler.vanderhoef@vibrantusa.com>
2017-02-12 01:59:23 -0800

    Initial build of stations API. Still needs more tests.

---
+++ Server/Backend.DataAccess/Repositories/StationRepository.cs
@@ -0,0 +1,101 @@
+﻿using Backend.DataAccess.Abstractions;
+using Backend.DataAccess.Repositories.DataSources;
+using Backend.Schemas;
+using Mobile_Rounds.ViewModels.Admin.Regions;
+using Mobile_Rounds.ViewModels.Regular.Region;
+using Mobile_Rounds.ViewModels.Regular.Station;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.DataAccess.Repositories
+{
+    /// <summary>
+    /// Represents a way to interface with Regions in the database.
+    /// </summary>
+    public sealed class StationRepository 
+        : AbstractRepository<StationModel, Station>
+    {
+        /// <summary>
+        /// Creates a new instance for working with database based regions.
+        /// </summary>
+        /// <param name="database">The database to use.</param>
+        public StationRepository(DatabaseContext database)
+            : base (new StationDataSource(database))
+        {
+        }
+
+        /// <inheritdoc />
+        public override Task<StationModel> DeleteAsync(StationModel toDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public override async Task<IEnumerable<StationModel>> GetAsync()
+        {
+            //TODO: Remove this sample implementation with a real one.
+            return await DataSource
+                //Get the records in order
+                .GetOrdered()
+                //convert records to view models 
+                .Select(r => new StationModel
+                {
+                    Id = r.Id,
+                    RegionId = r.RegionId,
+                    Name = r.Name
+                })
+                //load the data
+                .ToListAsync();
+
+        }
+
+        /// <inheritdoc />
+        public override Task<StationModel> GetSingleAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public override async Task<StationModel> InsertAsync(StationModel toCreate)
+        {
+            var model = BuildModel(toCreate);
+            var result = await DataSource.InsertAsync(model);
+            return BuildViewModel(result);
+        }
+
+        /// <inheritdoc />
+        public override async Task<StationModel> UpdateAsync(StationModel toUpdate)
+        {
+            var model = BuildModel(toUpdate);
+
+            var result = await DataSource.UpdateAsync(model);
+            return BuildViewModel(result);
+        }
+
+        protected override StationModel BuildViewModel(Station model)
+        {
+            if (model == null) return null;
+            return new StationModel
+            {
+                Id = model.Id,
+                RegionId = model.RegionId,
+                Name = model.Name
+            };
+        }
+
+        protected override Station BuildModel(StationModel model)
+        {
+            if (model == null) return null;
+            return new Station
+            {
+                Id = model.Id,
+                RegionId = model.RegionId,
+                Name = model.Name
+            };
+        }
+    }
+}
---
+++ Server/Backend.Tests/Repositories/StationRepositoryTests.cs
@@ -0,0 +1,139 @@
+﻿using Backend.DataAccess.Repositories;
+using Backend.Schemas;
+using Backend.Tests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mobile_Rounds.ViewModels.Regular.Station;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Tests.Repositories
+{
+    [TestClass]
+    public class StationRepositoryTests : BaseTestClass
+    {
+        public const string Category = "Repos";
+
+        private readonly Guid DefaultRegionId = Guid.Parse("{12255F6B-BA85-42F9-BD3B-1732860D075C}");
+
+        void InsertDefaultRegion()
+        {
+            Context.Regions.Add(new Region
+            {
+                Id = DefaultRegionId,
+                Name = "Default Region"
+            });
+            Context.SaveChanges();
+        }
+
+        [TestInitialize]
+        public void Startup()
+        {
+            InsertDefaultRegion();
+        }
+
+        [TestMethod]
+        [TestCategory(Category)]
+        public async Task Gets_In_Order_By_Name()
+        {
+            var repo = new StationRepository(Context);
+
+            Context.Stations.Add(new Station
+            {
+                Id = Guid.NewGuid(),
+                Name = "My Custom Station",
+                RegionId = DefaultRegionId
+            });
+
+            Context.Stations.Add(new Station
+            {
+                Id = Guid.NewGuid(),
+                Name = "A different name",
+                RegionId = DefaultRegionId
+            });
+            Context.SaveChanges();
+
+            var orderedList = await repo.GetAsync();
+
+            Assert.AreEqual(2, orderedList.Count());
+            Assert.AreEqual("A different name", orderedList.First().Name);
+            Assert.AreEqual("My Custom Station", orderedList.Last().Name);
+        }
+
+        [TestMethod]
+        [TestCategory(Category)]
+        public async Task Inserts_New()
+        {
+            var repo = new StationRepository(Context);
+
+            var newStation = new StationModel
+            {
+                Name = "My Custom Station",
+                RegionId = DefaultRegionId
+            };
+
+            var inserted = await repo.InsertAsync(newStation);
+
+            Assert.IsNotNull(inserted);
+            Assert.AreEqual(1, Context.Stations.Count());
+            Assert.AreEqual(inserted.Name, "My Custom Station");
+            Assert.AreEqual(newStation.RegionId, inserted.RegionId);
+            Assert.IsTrue(inserted.Id != Guid.Empty);
+        }
+
+        [TestMethod]
+        [TestCategory(Category)]
+        public async Task Cannot_Insert_Duplicate()
+        {
+            var repo = new StationRepository(Context);
+
+            var newStation = new StationModel
+            {
+                Name = "My Custom Station",
+                RegionId = DefaultRegionId
+            };
+
+            Context.Stations.Add(new Station
+            {
+                Id = Guid.NewGuid(),
+                Name = newStation.Name,
+                RegionId = DefaultRegionId
+            });
+            Context.SaveChanges();
+
+            var inserted = await repo.InsertAsync(newStation);
+
+            Assert.IsNull(inserted);
+            Assert.AreEqual(1, Context.Stations.Count());
+        }
+
+
+        [TestMethod]
+        [TestCategory(Category)]
+        public async Task Updates()
+        {
+            var repo = new StationRepository(Context);
+
+            var original = new StationModel
+            {
+                Id = Guid.Parse("{070F3379-2EE0-4EFB-A44F-600E485F8D6E}"),
+                Name = "My Custom Station",
+                RegionId = DefaultRegionId
+            };
+
+            var inserted = await repo.InsertAsync(original);
+            var updated = new StationModel
+            {
+                Id = inserted.Id,
+                Name = "My New Station",
+                RegionId = DefaultRegionId
+            };
+
+            var result = await repo.UpdateAsync(updated);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, Context.Stations.Count());
+            Assert.AreNotEqual(inserted.Name, result.Name);
+        }
+    }
+}
