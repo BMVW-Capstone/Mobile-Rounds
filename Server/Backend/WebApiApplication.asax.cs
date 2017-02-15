cc6f188baf86c0092de37ce85eaf0a7b68f363f4
Tyler Vanderhoef <tyler.vanderhoef@vibrantusa.com>
2017-02-15 01:56:35 -0800

    Slimmed the JSON response size by not sending nulls and empties.

--- Server/Backend/WebApiApplication.asax.cs
+++ Server/Backend/WebApiApplication.asax.cs
@@ -2,6 +2,7 @@
 // Copyright (c) SolarWorld Capstone Team. All rights reserved.
 // </copyright>
 
+using Newtonsoft.Json;
 using System.Web.Http;
 
 namespace Backend
@@ -17,6 +18,31 @@ namespace Backend
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            this.ConfigureJSON();
+        }
+
+        private void ConfigureJSON()
+        {
+            // register the default settings for converting items to JSON
+            // this is important because we spend a lot of bits just passing
+            // null or empty values. This is ALL for performance purposes.
+            HttpConfiguration config = GlobalConfiguration.Configuration;
+
+            // ignore whitespace and just print as small as possible.
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting =
+                Formatting.None;
+
+            // ignore missing properties/set them to null or empty.
+            config.Formatters
+                .JsonFormatter
+                .SerializerSettings
+                .MissingMemberHandling = MissingMemberHandling.Ignore;
+
+            // ignore null values.
+            config.Formatters
+                .JsonFormatter
+                .SerializerSettings
+                .NullValueHandling = NullValueHandling.Ignore;
         }
     }
 }
