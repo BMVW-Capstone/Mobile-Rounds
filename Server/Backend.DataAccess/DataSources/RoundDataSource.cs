7b7be0a4c93d034d4a3e31a9ec598598536d4ba2
Wyatt McGehee <wyatt.mcgehee@wsu.edu>
2017-02-15 10:57:30 -0800

    Rounds API complete?

---
+++ Server/Backend.DataAccess/DataSources/RoundDataSource.cs
@@ -0,0 +1,58 @@
+﻿using Backend.DataAccess.Abstractions;
+using Backend.Schemas;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.DataAccess.Repositories.DataSources
+{
+    internal sealed class RoundDataSource : AbstractDataSource<Round>
+    {
+        public RoundDataSource(DatabaseContext ctx) : base(ctx)
+        {
+        }
+
+        public override IQueryable<Round> Get()
+        {
+            return Database.Rounds
+                .Where(s => s.IsMarkedAsDeleted == false);
+        }
+
+        public override IOrderedQueryable<Round> GetOrdered()
+        {
+            // BY ID FOR NOW
+            return Get().OrderBy(r => r.Id);
+        }
+
+        public override async Task<Round> InsertAsync(Round toCreate)
+        {
+            toCreate.Id = Guid.NewGuid();
+
+            var tracked = Database.Rounds.Add(toCreate);
+            if (await SaveAsync())
+            {
+                return tracked;
+            }
+            return null;
+        }
+
+        public override async Task<Round> UpdateAsync(Round toUpdate)
+        {
+            var tracked = Database.Rounds.Find(toUpdate.Id);
+            if (tracked == null) return null;
+
+            tracked.AssignedTo = toUpdate.AssignedTo;
+            tracked.Region = toUpdate.Region;
+            tracked.RegionId = toUpdate.RegionId;
+            tracked.RoundHour = toUpdate.RoundHour;
+            tracked.StartTime = toUpdate.StartTime;
+            tracked.EndTime = toUpdate.EndTime;
+
+            if (await SaveAsync())
+            {
+                return tracked;
+            }
+            return null;
+        }
+    }
+}
