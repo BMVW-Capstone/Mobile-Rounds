62bfe7fd3e155d12189536e47c7dfb12fb06ac95
Tyler Vanderhoef <tyler.vanderhoef@vibrantusa.com>
2017-02-14 21:30:20 -0800

    Added specification API. Also updated documentation.

---
+++ Server/Backend.Tests/Controllers/SpecificationsControllerTests.cs
@@ -0,0 +1,256 @@
+﻿using Backend.Controllers;
+using Backend.Schemas;
+using Backend.Tests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mobile_Rounds.ViewModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Backend.Tests
+{
+    [TestClass]
+    public class SpecificationsControllerTests : BaseTestClass
+    {
+        public const string Category = "Controllers";
+
+        private Item SetupContext()
+        {
+            var region = new Region
+            {
+                Id = Guid.NewGuid(),
+                Name = "Region"
+            };
+            var station = new Station
+            {
+                Id = Guid.NewGuid(),
+                Name = "Station",
+                IsMarkedAsDeleted = false,
+                RegionId = region.Id
+            };
+            var item = new Item
+            {
+                Id = Guid.NewGuid(),
+                IsMarkedAsDeleted = false,
+                Name = "My Item",
+                Meter = "The Meter",
+                StationId = station.Id
+            };
+
+            var unit = new UnitOfMeasure
+            {
+                Id = Guid.NewGuid(),
+                Name = "Celcius",
+                Abbreviation = "C"
+            };
+
+            var between = new ComparisonType
+            {
+                Name = ComparisonType.Between
+            };
+
+            var either = new ComparisonType
+            {
+                Name = ComparisonType.Either
+            };
+            Context.ComparisonTypes.Add(between);
+            Context.ComparisonTypes.Add(either);
+            Context.UnitsOfMeasure.Add(unit);
+            Context.Regions.Add(region);
+            Context.Stations.Add(station);
+            Context.Items.Add(item);
+            Context.SaveChanges();
+            return item;
+        }
+
+        [TestMethod]
+        [TestCategory(Category)]
+        public async Task POST_Is_OK()
+        {
+            var controller = new SpecificationsController(Context);
+            ConfigureRequest(controller);
+
+            var item = SetupContext();
+
+            var model = new SpecificationModel
+            {
+                UnitOfMeasureId = Context.UnitsOfMeasure.First().Id,
+                ComparisonType = ComparisonType.Between,
+                LowerBound = "0",
+                UpperBound=  "10",
+                Id = item.Id
+            };
+
+            var result = await GetResponse(controller.Post(model));
+
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        [TestMethod]
+        [TestCategory(Category)]
+        public async Task POST_Inserts_Records()
+        {
+            var controller = new SpecificationsController(Context);
+            ConfigureRequest(controller);
+
+            var initialCount = Context.Specifications.Count();
+
+            var item = SetupContext();
+
+            var model = new SpecificationModel
+            {
+                UnitOfMeasureId = Context.UnitsOfMeasure.First().Id,
+                ComparisonType = ComparisonType.Between,
+                LowerBound = "0",
+                UpperBound = "10",
+                Id = item.Id
+            };
+
+            var result = await GetData<SpecificationModel>(controller.Post(model));
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(model.UnitOfMeasureId, result.UnitOfMeasureId);
+            Assert.AreEqual(model.ComparisonType, result.ComparisonType);
+            Assert.AreEqual(model.LowerBound, result.LowerBound);
+            Assert.AreEqual(model.UpperBound, result.UpperBound);
+            Assert.AreNotEqual(Guid.Empty, result.Id);
+        }
+
+        [TestMethod]
+        [TestCategory(Category)]
+        public async Task POST_Fails_Duplicate_Records()
+        {
+            var controller = new SpecificationsController(Context);
+            ConfigureRequest(controller);
+
+            var item = SetupContext();
+
+            var model = new SpecificationModel
+            {
+                UnitOfMeasureId = Context.UnitsOfMeasure.First().Id,
+                ComparisonType = ComparisonType.Between,
+                LowerBound = "0",
+                UpperBound = "10",
+                Id = item.Id
+            };
+
+            Context.Specifications.Add(new Specification
+            {
+                ItemId = item.Id,
+                UnitId = model.UnitOfMeasureId,
+                ComparisionTypeName = model.ComparisonType,
+                LowerBoundValue = model.LowerBound,
+                UpperBoundValue = model.UpperBound
+            });
+            Context.SaveChanges();
+
+            var result = await GetResponse(controller.Post(model));
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [TestMethod]
+        [TestCategory(Category)]
+        public async Task PUT_Is_Bad_Request_Null_Data()
+        {
+            var controller = new SpecificationsController(Context);
+            ConfigureRequest(controller);
+
+            var result = await GetResponse(controller.Put(null));
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [TestMethod]
+        [TestCategory(Category)]
+        public async Task PUT_Is_Bad_Request_Missing_Id()
+        {
+            var controller = new SpecificationsController(Context);
+            ConfigureRequest(controller);
+
+            var model = new SpecificationModel();
+
+            var result = await GetResponse(controller.Put(model));
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [TestMethod]
+        [TestCategory(Category)]
+        public async Task PUT_Is_OK()
+        {
+            var controller = new SpecificationsController(Context);
+            ConfigureRequest(controller);
+
+            var item = SetupContext();
+
+            var model = new SpecificationModel
+            {
+                Id = item.Id,
+                UnitOfMeasureId = Context.UnitsOfMeasure.First().Id,
+                ComparisonType = ComparisonType.Between,
+                LowerBound = "0",
+                UpperBound = "10"
+            };
+
+            //setup database record
+            Context.Specifications.Add(new Specification
+            {
+                ItemId = item.Id,
+                UnitId = model.UnitOfMeasureId,
+                ComparisionTypeName = model.ComparisonType,
+                LowerBoundValue = model.LowerBound,
+                UpperBoundValue = model.UpperBound
+            });
+            Context.SaveChanges();
+
+            var result = await GetResponse(controller.Put(model));
+
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        [TestMethod]
+        [TestCategory(Category)]
+        public async Task PUT_Updates_Data()
+        {
+            var controller = new SpecificationsController(Context);
+            ConfigureRequest(controller);
+
+            var item = SetupContext();
+
+            var model = new SpecificationModel
+            {
+                Id = item.Id,
+                UnitOfMeasureId = Context.UnitsOfMeasure.First().Id,
+                ComparisonType = ComparisonType.Either,
+                LowerBound = "True",
+                UpperBound = "False",
+                IsDeleted = true
+            };
+
+            //setup database record
+            Context.Specifications.Add(new Specification
+            {
+                ItemId = item.Id,
+                UnitId = model.UnitOfMeasureId,
+                ComparisionTypeName = model.ComparisonType,
+                LowerBoundValue = model.LowerBound,
+                UpperBoundValue = model.UpperBound
+            });
+            Context.SaveChanges();
+
+            var result = await GetData<SpecificationModel>(controller.Put(model));
+
+            Assert.AreEqual(model.Id, result.Id);
+            Assert.AreEqual(model.UnitOfMeasureId, result.UnitOfMeasureId);
+            Assert.AreEqual(model.ComparisonType, result.ComparisonType);
+            Assert.AreEqual(model.LowerBound, result.LowerBound);
+            Assert.IsTrue(result.IsDeleted);
+        }
+    }
+}
---
+++ Server/Backend/Controllers/SpecificationsController.cs
@@ -0,0 +1,79 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Backend.DataAccess.Abstractions;
+using Backend.DataAccess.Repositories;
+using Backend.Schemas;
+using Mobile_Rounds.ViewModels.Models;
+using Swashbuckle.Swagger.Annotations;
+
+namespace Backend.Controllers
+{
+    /// <summary>
+    /// The entrypoint into the <see cref="Specification"/> resources.
+    /// </summary>
+    [RoutePrefix("api/specifications")]
+    [Authorize]
+    public class SpecificationsController : ApiController
+    {
+        private const string SwaggerName = "Specifications";
+
+        private readonly IRepository<SpecificationModel> datasource;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpecificationsController"/> class.
+        /// </summary>
+        /// <param name="database">The database used for operations.</param>
+        public SpecificationsController(DatabaseContext database)
+        {
+            this.datasource = new SpecificationRepository(database);
+        }
+
+        /// <summary>
+        /// Inserts a given <see cref="Specification"/> using the values
+        /// from the <see cref="SpecificationModel"/>.
+        /// </summary>
+        /// <param name="newModel">The object with the values to insert.</param>
+        /// <returns>The newly updated specification. If the update failed, returns null.</returns>
+        [Route("")]
+        [SwaggerOperation(Tags = new[] { SwaggerName })]
+        public async Task<IHttpActionResult> Post(SpecificationModel newModel)
+        {
+            if (newModel == null)
+            {
+                return this.BadRequest("Model must not be null.");
+            }
+
+            var results = await this.datasource.InsertAsync(newModel);
+            if (results == null)
+            {
+                return this.BadRequest("The record could not be inserted.");
+            }
+
+            return this.Ok(results);
+        }
+
+        /// <summary>
+        /// Updates a given <see cref="Specification"/> using the values
+        /// from the <see cref="SpecificationModel"/>.
+        /// </summary>
+        /// <param name="updated">The object with the updated values.</param>
+        /// <returns>The newly updated spec. If the update failed, returns null.</returns>
+        [Route("")]
+        [SwaggerOperation(Tags = new[] { SwaggerName })]
+        public async Task<IHttpActionResult> Put(SpecificationModel updated)
+        {
+            if (updated == null)
+            {
+                return this.BadRequest("Model must not be null.");
+            }
+
+            var results = await this.datasource.UpdateAsync(updated);
+            if (results == null)
+            {
+                return this.BadRequest("The record could not be updated.");
+            }
+
+            return this.Ok(results);
+        }
+    }
+}
