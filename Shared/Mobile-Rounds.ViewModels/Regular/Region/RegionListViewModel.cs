6e6857e733d4caf83e53860a72ac48cffed16bf7
Chris Willette <christopher.willette@gmail.com>
2017-02-22 02:30:28 -0800

    fiddling with the regions
    getting some strange error. makes it seem like the syntax that the
    internet as a whole agrees on is not on good terms with VS. but that's
    par for the course.

--- Shared/Mobile-Rounds.ViewModels/Regular/Region/RegionListViewModel.cs
+++ Shared/Mobile-Rounds.ViewModels/Regular/Region/RegionListViewModel.cs
@@ -3,7 +3,11 @@
 // </copyright>
 
 using Mobile_Rounds.ViewModels.Shared;
+using Mobile_Rounds.ViewModels.Models;
+using Mobile_Rounds.ViewModels.Admin;
 using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+
 
 namespace Mobile_Rounds.ViewModels.Regular.Region
 {
@@ -33,8 +37,14 @@ namespace Mobile_Rounds.ViewModels.Regular.Region
         public RegionListViewModel()
         {
             this.Regions = new ObservableCollection<RegionModelSource>();
-            this.Regions.Add(new RegionModelSource() { Name = "North Region" });
-            this.Regions.Add(new RegionModelSource() { Name = "South Region" });
+            //this.Regions.Add(new RegionModelSource() { Name = "North Region" });
+            //this.Regions.Add(new RegionModelSource() { Name = "South Region" });
+
+
+            //gotta figure out how to pull the region names out and do the thing with new RegionModelSource()
+            Newtonsoft.Json.JsonConvert.DeserializeObject<RegionModel>(File.ReadAllText(@"ms-appx:///region_test.json"));
+            //i assume the deserialization will nicely append to this.Regions, but i don't know because errors.
+            //i'd love to figure out wth is up with the error on File.ReadAllText, because newtonsoft's documentation sport's that same thing
         }
 
         private RegionModelSource region;
